233e825f4099335a6f462cbfb67cc73e6bf65dcc
ovrmrw <t.ngc.0403@gmail.com>
2016-05-03 07:23:42 +0900

    Fixed

--- config.js
+++ config.js
@@ -5,23 +5,18 @@
 System.config({
   baseURL: '/',
   defaultJSExtensions: true,
-  
+
   // babelトランスパイラを使います。ブラウザでbabel-core/5.8.34/browser.min.jsをロードしておくこと。
   // babel6ではなく5を使うことに注意してください。
   transpiler: 'babel',
   map: {
     app: "src", // System.import('app')と書いたらsrcフォルダを参照するという意味。
-    
-    'zone.js': 'node_modules/zone.js/dist/zone.js', // zone.jsはファイルを直接指定する。
-    
+
     // 下記数行はpackagesの記述とセットで書くことでangular2,rxjsのモジュールを動的にロードできます。
     //'angular2': 'node_modules/angular2',
-    '@angular/core': 'node_modules/@angular/core',
-    '@angular/common': 'node_modules/@angular/common',
-    '@angular/compiler': 'node_modules/@angular/compiler',
-    '@angular/platform-browser': 'node_modules/@angular/platform-browser',
-    '@angular/platform-browser-dynamic': 'node_modules/@angular/platform-browser-dynamic',
-    'rxjs': 'node_modules/rxjs',    
+    '@angular': 'node_modules/@angular',
+    'rxjs': 'node_modules/rxjs',
+    'zone.js': 'node_modules/zone.js',
   },
 
   // mapでファイルではなくフォルダを指定した場合、packagesの設定も合わせて必要になります。
@@ -35,7 +30,7 @@ System.config({
     // mapでフォルダを指定した場合はこれをセットで書かないとangular2,rxjsのモジュールを動的にロードできません。
     //angular2: {},
     '@angular/core': {
-      main: 'index'
+      main: 'index' // import '@angular/core';と書いたら node_modules/@angular2/core/index.js を参照するという意味。
     },
     '@angular/common': {
       main: 'index'
@@ -50,5 +45,6 @@ System.config({
       main: 'index'
     },
     rxjs: {},
+    'zone.js': {}
   }
 });
